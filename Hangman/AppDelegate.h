a393ad9b38cd7ed0becf0884c4fe9186c5551bbe
William Wu <williamwu.2k12@gmail.com>
2013-10-13 15:42:33 -0700

    Initial Commit

---
+++ Hangman/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Hangman
+//
+//  Created by William Wu on 10/13/13.
+//  Copyright (c) 2013 William Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Hangman/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Hangman
+//
+//  Created by William Wu on 10/13/13.
+//  Copyright (c) 2013 William Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
